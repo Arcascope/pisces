e40e25c63c6f2a0c1be9c4bd45b849cf75318ddf
Eric Canton <eric.d.canton@gmail.com>
2025-01-06 18:27:25 -0600

    mid experiment commit. experiment id should contain epochs=25 change.

--- examples/RGB_Spectrograms/plot_wasa_csv.py
+++ examples/RGB_Spectrograms/plot_wasa_csv.py
@@ -13,15 +13,17 @@ plt.rcParams.update({'font.size': 14})
 
 # Group by test_id and plot
 for test_id, group in df.groupby('test_id'):
-    plt.plot(group['experiment_id'], group['wasa95'], color='tab:blue', alpha=0.5)
+    plt.plot(group['experiment_id'], group['wasa95'], 
+            #  color='tab:blue',
+             alpha=0.5)
 
 # Now plot the median trend line
 median = df.groupby('experiment_id').median()
-plt.plot(median.index, median['wasa95'], label='Median', color='red', linewidth=3, linestyle='--')
+plt.plot(median.index, median['wasa95'], label='Median', color='black', linewidth=4, linestyle='--')
 plt.fill_between(median.index, median['wasa95'], color='gray', alpha=0.3)
 
 mean = df.groupby('experiment_id').mean()
-plt.plot(mean.index, mean['wasa95'], label='Mean', color='orange', linewidth=2, linestyle=':')
+plt.plot(mean.index, mean['wasa95'], 'x-', label='Mean', color='black', linewidth=2)
 
 plt.xlabel('Experiment ID')
 plt.ylabel('wasa95')
