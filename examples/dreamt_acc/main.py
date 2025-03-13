613c0f11cd964bfec6a276e6dfd3ee3a9be86e0e
Eric Canton <eric.d.canton@gmail.com>
2025-03-13 16:48:33 -0500

    loads of work building classifier to score seconds-->seconds directly

--- examples/dreamt_acc/main.py
+++ examples/dreamt_acc/main.py
@@ -8,6 +8,9 @@ from scipy import signal
 import seaborn as sns
 import numpy as np
 import polars as pl
+from examples.dreamt_acc.constants import *
+from examples.dreamt_acc.conv2d_net import train_loocv
+from examples.dreamt_acc.preprocess import Preprocessed
 from pisces.data_sets import DataSetObject
 from tqdm import tqdm
 
@@ -15,91 +18,6 @@ from tqdm import tqdm
 sns.set_theme('notebook', style='darkgrid')
 
 # Set up constants and locations
-DATA_DIR = '/home/eric/Engineering/Work/pisces/data'
-FEATURE_COLS = ['ACC_X', 'ACC_Y', 'ACC_Z']
-TIMESTAMP_COL = 'TIMESTAMP'
-LABEL_COL = 'Sleep_Stage'
-NEW_LABEL_COL = 'PSG'
-SELECT_COLS = [TIMESTAMP_COL, *FEATURE_COLS, LABEL_COL]
-TIMESTAMP_HZ = 64
-TIMESTAMP_DT = 1/TIMESTAMP_HZ
-
-ACC_MAX_IDX = 2 ** 22
-PSG_MAX_IDX = 2 ** 16
-
-LABEL_MAP = {
-    'W': 0,
-    'N1': 1,
-    'N2': 1,
-    'N3': 2,
-    'R': 3,
-    'Missing': -1,
-    'P': 0
-}
-mapping_df = pl.DataFrame({
-    LABEL_COL: list(LABEL_MAP.keys()),
-    NEW_LABEL_COL: list(LABEL_MAP.values())
-})
-
-@dataclass
-class STFT:
-    f: np.ndarray
-    t: np.ndarray
-    Zxx: np.ndarray
-
-    @property
-    def shape(self):
-        return self.Zxx.shape
-    
-    def specgram(self, freq_n_tile_clamp: float = 0.0):
-        """Produces the absolute value of the STFT, 
-        with optional clamping of the values according to
-        the given percentile from top/bottom
-        """
-        abs_array = np.log10(np.abs(self.Zxx) ** 2 + 1e-6)
-        if freq_n_tile_clamp > 0:
-            abs_array = np.clip(abs_array, 
-                                np.percentile(abs_array, freq_n_tile_clamp),
-                                np.percentile(abs_array, 100 - freq_n_tile_clamp))
-        return abs_array
-    
-    def plot(self, ax=None) -> plt.Axes:
-        if ax is None:
-            fig, ax = plt.subplots()
-        ax.imshow(self.specgram(freq_n_tile_clamp=0.05), 
-                  aspect='auto', origin='lower', extent=[self.t[0], self.t[-1], self.f[0], self.f[-1]])
-        ax.set_ylabel('Frequency [Hz]')
-        ax.set_xlabel('Time [s]')
-        return ax
-    
-    @classmethod
-    def from_acc(cls, 
-                 x: np.ndarray, 
-                 fs: float = None, 
-                 nperseg: int = None, 
-                 noverlap: int = None):
-        fs = fs or TIMESTAMP_HZ
-        nperseg = nperseg or 4 * fs
-        noverlap = noverlap or nperseg // 4
-        window = signal.windows.hann(nperseg)
-        # fft_taker = signal.ShortTimeFFT(win=window, hop=nperseg - noverlap, fs=fs)
-        # f, t, Zxx = fft_taker.stft(np.linalg.norm(x, axis=-1))
-        f, t, Zxx = signal.stft(np.linalg.norm(x, axis=-1), fs=fs, window=window, nperseg=nperseg, noverlap=noverlap)
-
-        return cls(f, t, Zxx)
-
-
-@dataclass
-class Preprocessed:
-    x: np.ndarray
-    y: np.ndarray
-    x_spec: STFT = None
-
-    def compute_specgram(self):
-        self.x_spec = STFT.from_acc(self.x)
-
-
-
 def compress_in_memory(x):
     return zlib.compress(pickle.dumps(x))
 
@@ -130,6 +48,7 @@ def preprocess_data(
         df = df.join(mapping_df, on=LABEL_COL).drop(LABEL_COL)
         x = df[FEATURE_COLS].to_numpy()
         y = df[NEW_LABEL_COL].to_numpy()
+        del df
         n_pad = TIMESTAMP_HZ - (x.shape[0] % TIMESTAMP_HZ)
         x = np.pad(x, ((0, n_pad), (0, 0)), mode='constant')
         y = np.pad(y, (0, n_pad), mode='constant')
@@ -150,6 +69,7 @@ def preprocess_data(
         x_pickled_c = compress_in_memory(x)
         y_pickled_c = compress_in_memory(y)
         prepro_data[d] = Preprocessed(x_pickled_c, y_pickled_c)
+        del x_pickled_c, y_pickled_c
 
     print("Saving preprocessed data")
     np.savez('dreamt_prepro_data.npz', prepro_data)
@@ -161,6 +81,7 @@ def make_beautiful_specgram_plot(prepro_x_y: Preprocessed):
     if prepro_x_y.x_spec is None:
         prepro_x_y.compute_specgram()
     prepro_x_y.x_spec.plot(ax[0])
+    print("Spec shape:", prepro_x_y.x_spec.shape)
 
     y_plot = prepro_x_y.y
     sns.lineplot(x=np.arange(len(y_plot)), y=y_plot, ax=ax[1])
@@ -193,35 +114,32 @@ if __name__ == '__main__':
     #     EXCLUDE_THRESHOLD
     # )
     prepro_data = np.load('dreamt_prepro_data.npz', allow_pickle=True)['arr_0'].item()
-    log_2_max_x = 1
-    log_2_max_y = 1
 
     images_dir = Path(__file__).resolve().parent / 'images'
     images_dir.mkdir(exist_ok=True)
     for k, v in prepro_data.items():
         x = decompress_in_memory(v.x)
         y = decompress_in_memory(v.y)
-        print(k, x.shape, y.shape)
-        print(f" => {len(x) / TIMESTAMP_HZ / 3600:.1f} hours of data")
 
-        log_len_x = np.log2(len(x))
-        log_len_y = np.log2(len(y))
-        if log_len_x > log_2_max_x:
-            log_2_max_x = log_len_x
-        if log_len_y > log_2_max_y:
-            log_2_max_y = log_len_y
         
-        prepro_data[k] = Preprocessed(x, y)
-        fig, ax = make_beautiful_specgram_plot(prepro_data[k])
+        prepro_k = Preprocessed(x, y)
+        prepro_k.compute_specgram()
+        print(k, prepro_k.x_spec.shape, prepro_k.y.shape)
+        if prepro_k.x_spec.shape[0] != PSG_MAX_IDX:
+            print(f"Padding {k} to {PSG_MAX_IDX}")
+        # print(f" => {len(x) / TIMESTAMP_HZ / 3600:.1f} hours of data")
+        prepro_data[k] = prepro_k
 
-        print("Saving image to", images_dir)
-        plt.savefig(images_dir /  f'{k}_specgram.png', dpi=300)
-        plt.close(fig)
+        # fig, ax = make_beautiful_specgram_plot(prepro_data[k])
 
-
-    print(f"Max log2(x) = {log_2_max_x:.1f}")
-    print(f"Max log2(y) = {log_2_max_y:.1f}")
-
+        # print("Saving image to", images_dir)
+        # plt.savefig(images_dir /  f'{k}_specgram.png', dpi=300)
+        # plt.close(fig)
     
+    # stack = np.array([prepro_data[k].x_spec.Zxx for k in prepro_data])
 
-    
+    results = train_loocv(
+        list(prepro_data.values()), 
+        num_epochs=20, 
+        lr=1e-3
+    )
---
+++ examples/dreamt_acc/preprocess.py
@@ -0,0 +1,101 @@
+
+
+from dataclasses import dataclass
+
+from matplotlib import pyplot as plt
+import numpy as np
+from scipy import signal
+
+from examples.dreamt_acc.constants import MASK_VALUE, PSG_MAX_IDX, TIMESTAMP_HZ
+
+
+@dataclass
+class STFT:
+    f: np.ndarray
+    t: np.ndarray
+    Zxx: np.ndarray
+
+    @property
+    def shape(self):
+        return self.Zxx.shape
+    
+    def specgram(self, freq_n_tile_clamp: float = 0.0):
+        """Produces the absolute value of the STFT, 
+        with optional clamping of the values according to
+        the given percentile from top/bottom
+        """
+        abs_array = np.log10(np.abs(self.Zxx) ** 2 + 1e-6)
+        if freq_n_tile_clamp > 0:
+            abs_array = np.clip(abs_array, 
+                                np.percentile(abs_array, freq_n_tile_clamp),
+                                np.percentile(abs_array, 100 - freq_n_tile_clamp))
+        return abs_array
+    
+    def plot(self, ax=None) -> plt.Axes:
+        if ax is None:
+            fig, ax = plt.subplots()
+        ax.imshow(self.specgram(freq_n_tile_clamp=0.05), 
+                  aspect='auto', origin='lower', extent=[self.t[0], self.t[-1], self.f[0], self.f[-1]])
+        ax.set_ylabel('Frequency [Hz]')
+        ax.set_xlabel('Time [s]')
+        return ax
+    
+    @classmethod
+    def from_acc(cls, 
+                 x: np.ndarray, 
+                 fs: float = None, 
+                 nperseg: int = None, 
+                 noverlap: int = None):
+        fs = fs or TIMESTAMP_HZ
+        nperseg = nperseg or 4 * fs
+        noverlap = nperseg - fs
+        window = signal.windows.hann(nperseg)
+        # fft_taker = signal.ShortTimeFFT(win=window, hop=nperseg - noverlap, fs=fs)
+        # f, t, Zxx = fft_taker.stft(np.linalg.norm(x, axis=-1))
+        x_norm = np.linalg.norm(x, axis=-1)
+        x_diff = np.diff(x_norm)
+        f, t, Zxx = signal.stft(x_diff, fs=fs, window=window, nperseg=nperseg, noverlap=noverlap)
+
+        return cls(f, t, Zxx.T)
+
+
+@dataclass
+class Preprocessed:
+    x: np.ndarray
+    y: np.ndarray
+    x_spec: STFT = None
+
+    def compute_specgram(self, pad_to_psg_max_idx: bool = True):
+        self.x_spec = STFT.from_acc(self.x)
+        if pad_to_psg_max_idx:
+            self.pad_to_psg_max_idx()
+    
+    def pad_to_psg_max_idx(self):
+        if self.x_spec is None:
+            return
+        
+        if self.x_spec.shape[0] <= PSG_MAX_IDX:
+            self.x_spec.Zxx = np.pad(
+                self.x_spec.Zxx,
+                ((0, PSG_MAX_IDX - self.x_spec.shape[0]), (0, 0)),
+                mode='constant',
+                constant_values=0
+            )
+            # extend the time axis
+            self.x_spec.t = np.linspace(
+                self.x_spec.t[0],
+                # not / TIMESTAMP_HZ, x_spec.t is in seconds
+                self.x_spec.t[-1] + (PSG_MAX_IDX - self.x_spec.shape[0]),
+                PSG_MAX_IDX)
+        elif self.x_spec.shape[0] > PSG_MAX_IDX:
+            self.x_spec.Zxx = self.x_spec.Zxx[:PSG_MAX_IDX, :]
+            self.x_spec.t = self.x_spec.t[:PSG_MAX_IDX]
+
+        if self.y.shape[0] < PSG_MAX_IDX:
+            self.y = np.pad(
+                self.y,
+                (0, PSG_MAX_IDX - self.y.shape[0]),
+                mode='constant',
+                constant_values=MASK_VALUE)
+        elif self.y.shape[0] > PSG_MAX_IDX:
+            self.y = self.y[:PSG_MAX_IDX]
