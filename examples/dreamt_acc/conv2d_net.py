0808ac27eb27cc3859c17aeefc971be644fd5f4b
Eric Canton <eric.d.canton@gmail.com>
2025-03-18 16:21:35 -0500

    cpu numpy at the right time

--- examples/dreamt_acc/conv2d_net.py
+++ examples/dreamt_acc/conv2d_net.py
@@ -155,8 +155,8 @@ def wasa(model, X_test_tensor, y_test_tensor, target_sleep_acc) -> WASAResult:
 
         y_true = y_test_flat[valid_mask].cpu().numpy()  # ground truth labels
         
-        raw_outputs = torch.softmax(test_outputs, dim=1).cpu().numpy()      # predicted probabilities for class 1
-        valid_outputs = raw_outputs[0, 1, valid_mask]  # shape: (N,)
+        raw_outputs = torch.softmax(test_outputs, dim=1)      # predicted probabilities for class 1
+        valid_outputs = raw_outputs[0, 1, valid_mask].cpu().numpy()  # shape: (N,)
 
         # Use a binary search on threshold, starting halfway between probs.max() and probs.min()
         # Note that we're using logits potentially so we don't assume probs are in [0, 1].
