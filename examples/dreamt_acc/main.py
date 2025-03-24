e7017f5466cd1a3c6f28fda7627b5a4f8635796a
Eric Canton <eric.d.canton@gmail.com>
2025-03-24 13:03:36 -0500

    5 epochs,

--- examples/dreamt_acc/main.py
+++ examples/dreamt_acc/main.py
@@ -133,7 +133,7 @@ if __name__ == '__main__':
     time_start = time.time()
     results = train_loocv(
         prepro_values, 
-        num_epochs=4, 
+        num_epochs=5, 
         batch_size=2,
         experiment_results_csv=EXPERIMENT_RESULTS_CSV,
         lr=1e-3
